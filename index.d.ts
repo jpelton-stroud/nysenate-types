320d566fd6a73d75388025a15887d2981e63d00e
Joshua Pelton-Stroud <jpstroud@gmail.com>
2025-12-19 07:45:18 -0500

    updt

---
+++ index.d.ts
@@ -0,0 +1,12 @@
+/// <reference path="./api-response.d.ts" />
+/// <reference path="./members.d.ts" />
+/// <reference path="./bills.d.ts" />
+/// <reference path="./committees.d.ts" />
+/// <reference path="./agendas.d.ts" />
+/// <reference path="./calendars.d.ts" />
+/// <reference path="./laws.d.ts" />
+/// <reference path="./transcripts.d.ts" />
+/// <reference path="./agg_updates.d.ts" />
+
+export = NYSOpenLegislation;
+export as namespace NYSOpenLegislation;
